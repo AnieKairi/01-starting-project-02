ccc7136ec4ae7549b6c263056cbbb14ea12c306a
aniekairi.code@outlook.com <aniekairi.code@outlook.com>
2025-08-10 12:26:48 -0700

    section-03-46

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import { CORE_CONCEPTS } from './data.js'
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -20,11 +22,30 @@ function Header() {
   );
 }
 
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/> */}
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept image={CORE_CONCEPTS[1].image} title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
