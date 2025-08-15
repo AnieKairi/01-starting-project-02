cc4aca996c3b28ecdba833b6695c0dcbf6b65a9a
aniekairi.code@outlook.com <aniekairi.code@outlook.com>
2025-08-14 18:46:32 -0700

    section-03-54

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
@@ -5,8 +6,11 @@ import TabButton from './TabButton.jsx';
 
 function App() {
 
-  function handleSelect() {
-    console.log('hellow world');
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
@@ -26,12 +30,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={handleSelect}>Components</TabButton>
-          <TabButton onSelect={handleSelect}>JSX</TabButton>
-          <TabButton onSelect={handleSelect}>Props</TabButton>
-          <TabButton onSelect={handleSelect}>State</TabButton>
+          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
+        {selectedTopic}
       </main>
     </div>
   );
