848c1ed3c7d499f302f4952db4c645a50035407c
aniekairi.code@outlook.com <aniekairi.code@outlook.com>
2025-09-20 09:51:46 -0700

    section-03-55

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -35,6 +36,15 @@ function App() {
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
         {selectedTopic}
       </main>
